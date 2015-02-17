1cd186e8c1ddd99871129778a085cbb7f6b2835f
Jared Bridgham <k3b910@tds.net>
2015-02-16 21:20:58 -0500

    First Commit

---
+++ src/net/androidbootcamp/cityguide/Bridge.java
@@ -0,0 +1,12 @@
+package net.androidbootcamp.cityguide;
+
+import android.app.Activity;
+
+public class Bridge extends Activity {
+	@Override
+protected void onCreate(android.os.Bundle savedInstanceState) {
+	//TODO
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.bridge);
+};
+}
---
+++ src/net/androidbootcamp/cityguide/trolley.java
@@ -0,0 +1,13 @@
+package net.androidbootcamp.cityguide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class trolley extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.trolley);
+	}
+
+}
